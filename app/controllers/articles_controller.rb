b873232a6e7ff0f8a9230b2843f7fb7d7f099668
Sonia Hussain <sonia_ahm@icloud.com>
2023-05-30 21:23:52 +0100

    new and create methods have been create enabling users to create a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,31 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # POST /articles
+  def create
+    # Create a new article object with the parameters passed in
+    @article = Article.new(article_params)
+
+    # If the article is saved successfully, redirect to the show action
+    # for that article
+    if @article.save
+      redirect_to @article
+    else
+      # Otherwise, render the new template again
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    # Require the top-level key of "article" and permit the "title" and "body" keys
+    params.require(:article).permit(:title, :content)
+  end
 end
